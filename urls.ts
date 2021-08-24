9c12e66d40f6e41a055781942ec34c9a1df99142
yui540 <tabletennis540@gmail.com>
2021-08-24 16:37:55 +0900

    urls.tsの追加

---
+++ urls.ts
@@ -0,0 +1,11 @@
+const urls = {
+  index: '/',
+  news: '/news',
+  story: '/story',
+  charactor: '/charactor',
+  works: '/works',
+  shop: '/shop',
+  contact: '/contact',
+}
+
+export default urls
